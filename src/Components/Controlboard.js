afa1dee219c567b300ae67292935b74d3351b46e
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-03-18 15:20:35 +0100

    added buttons to controlboard

--- src/Components/Controlboard.js
+++ src/Components/Controlboard.js
@@ -6,24 +6,40 @@ class ControlBoard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            direction: "test"
+            direction: ""
         }
         this.handlClick = this.handlClick.bind(this);
     }
 
-    handlClick(){
-        axios.post('http://localhost:8080/api/navigation', {name: 'ola'})
-            .then(console.log("post requsert sent"))
+    handlClick(control){
+        this.setState({direction: control});
+        axios.post('http://localhost:8080/api/navigation', this.state.direction)
+            .then(console.log("post request sent"))
     }
 
     render(){
         return(
             <div>
-                <button onClick={this.handlClick}>
-                Post!
+              <div className="sensor-heading"><h2>Controlboard</h2></div>
+              <div className="sensor-panel">
+                <button onClick={() => this.handlClick("left")} className="sensor">
+                  Left
                 </button>
+                <button onClick={() => this.handlClick("right")} className="sensor">
+                  Right
+                </button>
+                <button onClick={() => this.handlClick("drive")} className="sensor">
+                  Drive
+                </button>
+                <button onClick={() => this.handlClick("reverse")} className="sensor">
+                  Reverse
+                </button>
+                <button onClick={() => this.handlClick("stop")} className="stop">
+                  STOP
+                </button>
+              </div>
             </div>
-        );
+          );
     }
 }
 export default ControlBoard;
