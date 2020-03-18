1c5eb7e523d669c3cdf88e3e1037ef14fb4dc74d
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-03-18 14:39:50 +0100

    handleClick function added to Controlboard.js

--- src/Components/Controlboard.js
+++ src/Components/Controlboard.js
@@ -8,12 +8,18 @@ class ControlBoard extends React.Component {
         this.state = {
             direction: "test"
         }
+        this.handlClick = this.handlClick.bind(this);
+    }
+
+    handlClick(){
+        axios.post('http://localhost:8080/api/navigation', {name: 'ola'})
+            .then(console.log("post requsert sent"))
     }
 
     render(){
         return(
             <div>
-                <button onClick={axios.post("http://localhost:8080/api/navigation", this.state.direction)}>
+                <button onClick={this.handlClick}>
                 Post!
                 </button>
             </div>
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     return(
       <div>
         <Sensors/>
+        <ControlBoard/>
       </div>
     )
   }
